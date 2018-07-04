5622b2fe81fb906f266bb0ab2622e6d9eae32b4e
Sachin Kumar Kamat <36402247+codeKAMAT@users.noreply.github.com>
2018-07-04 22:52:00 +0530

    Create Drawing a square without using loop.py

---
+++ Drawing a square without using loop.py
@@ -0,0 +1,17 @@
+import turtle
+
+square = turtle.Turtle()
+
+square.forward(50)
+square.right(90)     # Rotate clockwise by 90 degrees
+
+square.forward(50)
+square.right(90)
+
+square.forward(50)
+square.right(90)
+
+square.forward(50)
+square.right(90)
+
+turtle.done()
