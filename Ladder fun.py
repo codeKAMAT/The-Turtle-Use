829ce09540e2978ad1cbf82f42fc906611a93d09
Sachin Kumar Kamat <36402247+codeKAMAT@users.noreply.github.com>
2018-07-06 00:17:32 +0530

    Update Ladder fun.py

--- Ladder fun.py
+++ Ladder fun.py
@@ -34,18 +34,42 @@ for i in range(5):
     
 smart.left(180)
 smart.forward(50)
+smart.right(90)
 
 for i in range(5):
-    smart.right(90)
     smart.forward(5)
     smart.left(90)
     smart.forward(300)
     smart.right(90)
     smart.forward(5)
-    smart.left(90)
+    smart.right(90)
     smart.forward(300)
+    smart.left(90)
 
+smart.left(90)
+smart.forward(50)
+smart.right(90)
+
+for i in range(5):
+    smart.forward(5)
+    smart.left(90)
+    smart.forward(200)
+    smart.right(90)
+    smart.forward(5)
+    smart.right(90)
+    smart.forward(200)
+    smart.left(90)
     
+smart.left(90)
+smart.forward(50)
+smart.right(90)
 
-
-turtle.done()
+for i in range(5):
+    smart.forward(5)
+    smart.left(90)
+    smart.forward(100)
+    smart.right(90)
+    smart.forward(5)
+    smart.right(90)
+    smart.forward(100)
+    smart.left(90)
